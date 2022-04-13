692f13b114e420562ec8df31b3071635820b8f47
ghisi_note <guilhermeghisi14@gmail.com>
2022-04-13 20:21:32 -0300

    Testando copilot

---
+++ Estrutura de dados/Aula03/lib_ponto.h
@@ -0,0 +1,12 @@
+#ifndef LIB_PONTO_H_INCLUDED
+#define LIB_PONTO_H_INCLUDED
+
+typedef struct pont PONTO;
+
+PONTO * pto_cria(float x, float y);
+void pto_libera(PONTO * p);
+void pto_acessa(PONTO * p, float * x, float * y);
+void pto_atribui(PONTO * p, float x, float y);
+float pto_distancia(PONTO * p1, PONTO * p2);
+
+#endif // LIB_PONTO_H_INCLUDED
