f19f3e1e5425fd46f26437645ee55b91f16526e4
ghisi_note <guilhermeghisi14@gmail.com>
2022-04-13 22:04:21 -0300

    Não sei junta arrumado v.2

--- Estrutura de dados/Aula03/Ponto/main.c
+++ Estrutura de dados/Aula03/Ponto/main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <../lib_ponto/lib_ponto.h>
+#include "../lib_ponto/lib_ponto.h"
 
 int main(){
-    printf("Hello World\n");
-    PONTO *p;
-    PONTO *q;
-    float *x1, *y1;
-    float *x2, *y2;
-    p = pto_cria(1,2);
-    q = pto_cria(3.4,2.1);
+  PONTO *p;
+  PONTO *q;
+  p = pto_cria(1,2);
+    q = pto_cria(3,4);
+
+    float *x1, *y1, *x2, *y2;
 
     float d = pto_distancia(p,q);
-    printf("O ponto 1 e: %.2f, %.2f\n", x1,y1);
-    prtinf("O ponto 2 e: %.2f, %.2f\n", x2,y2);
-    printf("A distancia entre os pontos e: %.2f\n", d);
+    pto_acessa(p,x1,y1);
+    pto_acessa(q,x2,y2);
+    printf("distancia entre os pontos: %.2f\n",d);
     pto_libera(p);
     pto_libera(q);
     return 0;
